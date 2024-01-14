35877de7059bbded8bcd1aea8f718dc024b39112
Zhiheng Chen <czhdawang@163.com>
2024-01-15 00:02:53 +0800

    add files for template.

--- cpp/cppStartup/cppStartup/cppStartup.cpp
+++ cpp/cppStartup/cppStartup/cppStartup.cpp
@@ -4,11 +4,19 @@
 #include <iostream>
 #include "testSuiteForComplex.h"
 #include "MyString.h"
+#include"oop.h"
+/*
 
+
+
+
+*/
 int  main()
 {   
     char a[]{ 'a','b', '\0' };
     cout << MyString(a) << endl;
+
+    example2::testTT_param();
 
 }
 
